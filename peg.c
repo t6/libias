08df3a84a4072fb835aee539fe77661b72526548
Tobias Kortkamp <t@tobik.me>
2021-02-11 09:33:06 +0100

    Change approach a little...

--- peg.c
+++ peg.c
@@ -6,286 +6,290 @@
 
 #include "strnstr.c"
 
-struct Parser;
+struct PEG;
 
-typedef void (*CaptureFn)(struct Parser *, const char *, const char *, size_t, void *);
-typedef void (*MismatchFn)(struct Parser *, const char *, void *);
-typedef void (*RuleFn)(struct Parser *);
+typedef void (*CaptureFn)(struct PEG *, const char *, const char *, size_t, void *);
+typedef void (*MismatchFn)(struct PEG *, const char *, void *);
+typedef int (*RuleFn)(struct PEG *);
 
-enum ParserState {
+enum PEGState {
 	PARSER_OK,
 	PARSER_MISMATCH,
 };
 
-struct Parser {
+struct PEG {
 	const char *buf;
 	size_t pos;
 	size_t len;
-	enum ParserState state;
+	enum PEGState state;
 
 	CaptureFn on_capture;
 	MismatchFn on_mismatch;
 	void *userdata;
 };
 
-#define __CHECK(x, t, e)	if (x) { t; } else { e; }
-#define __BETWEEN(r, a, b, t, e)__CHECK(parser_match_between(parser, __func__, r, a, b), t, e)
-#define __CHAR(c, n, t, e)	__CHECK(parser_match_char(parser, __func__, c, n), t, e)
-#define __CHARF(f, n, t, e)	__CHECK(parser_match_char_f(parser, __func__, f, n), t, e)
-#define __EOS(t, e)		__CHECK(parser_match_eos(parser, __func__), t, e)
-#define __RANGE(a, b, t, e)	__CHECK(parser_match_range(parser, __func__, a, b), t, e)
-#define __MATCH(r, t, e)	__CHECK(parser_match_rule(parser, __func__, r), t, e)
-#define __STRING(s, t, e)	__CHECK(parser_match_string(parser, __func__, s), t, e)
-#define __THRU(c, t, e)		__CHECK(parser_match_thruto(parser, __func__, c, 1), t, e)
-#define __TO(c, t, e)		__CHECK(parser_match_thruto(parser, __func__, c, 0), t, e)
 #define ANY(r)			ATLEAST(r, 0)
-#define ATLEAST(r, n)		__BETWEEN(r, n, n, , return)
-#define BETWEEN(r, a, b)	__BETWEEN(r, a, b, , return)
-#define CHAR(c, n)		__CHAR(c, n, , return)
-#define CHARF(f, n)		__CHARF(f, n, , return)
-#define EOS()			__EOS(, return)
-#define MATCH(r)		__MATCH(r, , return)
-#define RANGE(a, b)		__RANGE(a, b, , return)
+#define ATLEAST(r, n)		peg_match_atleast(peg, __func__, r, n)
+#define BETWEEN(r, a, b)	peg_match_between(peg, __func__, r, a, b)
+#define CHAR(c)			peg_match_char(peg, __func__, c)
+#define CHARF(f)		peg_match_char_f(peg, __func__, f)
+#define EOS()			peg_match_eos(peg, __func__)
+#define MATCH(r)		peg_match_rule(peg, __func__, r)
+#define RANGE(a, b)		peg_match_range(peg, __func__, a, b)
+#define REPEAT(r, n)		peg_match_repeat(peg, __func__, r, n)
 #define SOME(rule)		ATLEAST(rule, 1)
-#define STRING(s)		__STRING(s, , return)
-#define THRU(c)			__THRU(c, , return)
-#define TO(c)			__TO(c, , return)
-#define TRY_ANY(r)		TRY_ATLEAST(c, 0)
-#define TRY_ATLEAST(r, n)	__BETWEEN(r, n, n, return, )
-#define TRY_BETWEEN(r, a, b)	__BETWEEN(r, a, b, return, )
-#define TRY_CHAR(c, n)		__CHAR(c, n, return, )
-#define TRY_CHARF(f, n)		__CHARF(f, n, return, )
-#define TRY_EOS()		__EOS(return, )
-#define TRY_MATCH(r)		__MATCH(r, return, )
-#define TRY_RANGE(a, b)		__RANGE(a, b, return, )
-#define TRY_SOME(r)		TRY_ATLEAST(rule, 1)
-#define TRY_STRING(s)		__STRING(s, return, )
-#define TRY_THRU(c)		__THRU(c, return, )
-#define TRY_TO(c)		__TO(c, return, )
-#define RULE(name)		void name(struct Parser *parser)
+#define STRING(s)		peg_match_string(peg, __func__, s)
+#define THRU(c)			peg_match_thruto(peg, __func__, c, 1)
+#define TO(c)			peg_match_thruto(peg, __func__, c, 0)
+#define RULE(name)		int name(struct PEG *peg)
 
 static void
-parser_capture(struct Parser *parser, const char *rule, size_t len)
+peg_capture(struct PEG *peg, const char *rule, size_t len)
 {
-	assert((parser->pos + len) <= parser->len);
-	if (parser->on_capture) {
-		parser->on_capture(parser, rule, parser->buf + parser->pos, len, parser->userdata);
+	assert((peg->pos + len) <= peg->len);
+	if (peg->on_capture) {
+		peg->on_capture(peg, rule, peg->buf + peg->pos, len, peg->userdata);
 	}
 }
 
 static int
-parser_mismatch(struct Parser *parser, const char *rule)
+peg_match_init(struct PEG *peg, const char *rule)
 {
-	parser->state = PARSER_MISMATCH;
-	if (parser->on_mismatch) {
-		parser->on_mismatch(parser, rule, parser->userdata);
-	}
-	return 0;
+	return peg->pos <= peg->len;
 }
 
 static int
-parser_match_ok(struct Parser *parser)
+peg_match_atleast(struct PEG *peg, const char *rule, RuleFn rulefn, int n)
 {
-	parser->state = PARSER_OK;
-	return 1;
-}
-
-static int
-parser_match_init(struct Parser *parser, const char *rule)
-{
-	if (parser->pos > parser->len) {
-		return parser_mismatch(parser, rule);
-	}
-	return parser_match_ok(parser);
-}
-
-static int
-parser_match_between(struct Parser *parser, const char *rule, RuleFn rulefn, int a, int b)
-{
-	if (!parser_match_init(parser, rule)) {
+	if (!peg_match_init(peg, rule)) {
 		return 0;
 	}
-	size_t pos = parser->pos;
+	size_t pos = peg->pos;
 	int i = 0;
 	while (1) {
-		rulefn(parser);
-		if (parser->state != PARSER_OK) {
+		if (!rulefn(peg)) {
 			break;
 		}
 		i++;
 	}
-	if (i < a || i > b) {
-		parser->pos = pos;
-		return parser_mismatch(parser, rule);
+	if (i < n) {
+		peg->pos = pos;
+		return 0;
 	}
-	return parser_match_ok(parser);
+	return 1;
 }
 
 static int
-parser_match_char(struct Parser *parser, const char *rule, char c, int n)
+peg_match_between(struct PEG *peg, const char *rule, RuleFn rulefn, int a, int b)
 {
-	if (!parser_match_init(parser, rule)) {
+	if (!peg_match_init(peg, rule)) {
 		return 0;
 	}
-	size_t len = parser->len - parser->pos;
-	if (len < n) {
-		return parser_mismatch(parser, rule);
-	}
-	size_t i = parser->pos;
-	for (int j = 0; j < n; j++, i++) {
-		if (parser->buf[i] != c) {
-			return parser_mismatch(parser, rule);
+	size_t pos = peg->pos;
+	int i = 0;
+	while (1) {
+		if (!rulefn(peg)) {
+			break;
 		}
+		i++;
 	}
-	parser->pos = i;
-	return parser_match_ok(parser);
+	if (i >= a && i < b) {
+		return 1;
+	} else {
+		peg->pos = pos;
+		return 0;
+	}
 }
 
 static int
-parser_match_char_f(struct Parser *parser, const char *rule, int (*f)(int), int n)
+peg_match_char(struct PEG *peg, const char *rule, char c)
 {
-	if (!parser_match_init(parser, rule)) {
+	if (!peg_match_init(peg, rule)) {
 		return 0;
 	}
-	size_t len = parser->len - parser->pos;
-	if (len < n) {
-		return parser_mismatch(parser, rule);
+	size_t len = peg->len - peg->pos;
+	if (len < 1) {
+		return 0;
 	}
-	size_t i = parser->pos;
-	for (int j = 0; j < n; j++, i++) {
-		if (!f(parser->buf[i])) {
-			return parser_mismatch(parser, rule);
-		}
+	if (peg->buf[peg->pos] == c) {
+		peg->pos++;
+		return 1;
+	} else {
+		return 0;
 	}
-	parser->pos = i;
-	return parser_match_ok(parser);
 }
 
 static int
-parser_match_eos(struct Parser *parser, const char *rule)
+peg_match_char_f(struct PEG *peg, const char *rule, int (*f)(int))
 {
-	if (!parser_match_init(parser, rule)) {
+	if (!peg_match_init(peg, rule)) {
 		return 0;
 	}
-	if (parser->pos != parser->len) {
-		return parser_mismatch(parser, rule);
+	size_t len = peg->len - peg->pos;
+	if (len < 1) {
+		return 0;
 	}
-	return parser_match_ok(parser);
+	if (f(peg->buf[peg->pos])) {
+		peg->pos++;
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
 static int
-parser_match_range(struct Parser *parser, const char *rule, char a, char b)
+peg_match_eos(struct PEG *peg, const char *rule)
 {
-	if (!parser_match_init(parser, rule)) {
+	return peg_match_init(peg, rule) && peg->pos == peg->len;
+}
+
+static int
+peg_match_range(struct PEG *peg, const char *rule, char a, char b)
+{
+	if (!peg_match_init(peg, rule)) {
 		return 0;
 	}
-	if ((parser->len - parser->pos) < 1) {
-		return parser_mismatch(parser, rule);
+	if ((peg->len - peg->pos) < 1) {
+		return 0;
 	}
-	char c = parser->buf[parser->pos];
+	char c = peg->buf[peg->pos];
 	if (a > c || c > b) {
-		printf("%c %c %c\n", a, b, c);
-		return parser_mismatch(parser, rule);
-	}
-	parser->pos++;
-	return parser_match_ok(parser);
-}
-static int
-parser_match_rule(struct Parser *parser, const char *rule, RuleFn rulefn)
-{
-	if (!parser_match_init(parser, rule)) {
 		return 0;
 	}
-	rulefn(parser);
-	if (parser->state == PARSER_MISMATCH) {
-		return parser_mismatch(parser, rule);
-	}
-	return parser_match_ok(parser);
+	peg->pos++;
+	return 1;
 }
 
 static int
-parser_match_string(struct Parser *parser, const char *rule, const char *needle)
+peg_match_repeat(struct PEG *peg, const char *rule, RuleFn rulefn, int n)
 {
-	if (!parser_match_init(parser, rule)) {
+	if (!peg_match_init(peg, rule)) {
+		return 0;
+	}
+	size_t pos = peg->pos;
+	for (int i = 0; i < n; i++) {
+		if (!rulefn(peg)) {
+			peg->pos = pos;
+			return 0;
+		}
+	}
+	return 1;
+}
+
+static int
+peg_match_rule(struct PEG *peg, const char *rule, RuleFn rulefn)
+{
+	if (!peg_match_init(peg, rule)) {
+		return 0;
+	}
+	return rulefn(peg);
+}
+
+static int
+peg_match_string(struct PEG *peg, const char *rule, const char *needle)
+{
+	if (!peg_match_init(peg, rule)) {
 		return 0;
 	}
 	size_t len = strlen(needle);
-	if ((parser->len - parser->pos) < len ||
-	    strncmp(parser->buf + parser->pos, needle, len) != 0) {
-		return parser_mismatch(parser, rule);
+	if ((peg->len - peg->pos) < len ||
+	    strncmp(peg->buf + peg->pos, needle, len) != 0) {
+		return 0;
 	}
-	parser->pos += len;
-	return parser_match_ok(parser);
+	peg->pos += len;
+	return 1;
 }
 
 static int
-parser_match_thruto(struct Parser *parser, const char *rule, const char *needle, int thru)
+peg_match_thruto(struct PEG *peg, const char *rule, const char *needle, int thru)
 {
-	if (!parser_match_init(parser, rule)) {
+	if (!peg_match_init(peg, rule)) {
 		return 0;
 	}
 	if (strcmp(needle, "") == 0) {
-		return parser_mismatch(parser, rule);
+		return 0;
 	}
 
-	char *ptr = strnstr(parser->buf + parser->pos, needle, parser->len);
+	char *ptr = strnstr(peg->buf + peg->pos, needle, peg->len);
 	if (ptr == NULL) {
-		return parser_mismatch(parser, rule);
+		return 0;
 	}
 
 	size_t offset = 0;
 	if (thru) {
 		offset = strlen(needle);
 	}
-	size_t len = ptr - (parser->buf + parser->pos) + offset;
-	if (parser->on_capture) {
-		parser_capture(parser, rule, len);
+	size_t len = ptr - (peg->buf + peg->pos) + offset;
+	if (peg->on_capture) {
+		peg_capture(peg, rule, len);
 	}
-	parser->pos += len;
+	peg->pos += len;
 
-	return parser_match_ok(parser);
+	return 1;
 }
 
-struct Parser *
-parser_new(const char *buf, size_t len, CaptureFn on_capture, MismatchFn on_mismatch)
+struct PEG *
+peg_new(const char *buf, size_t len, CaptureFn on_capture, MismatchFn on_mismatch)
 {
-	struct Parser *parser = malloc(sizeof(struct Parser));
-	if (parser == NULL) {
-		return NULL;
-	}
-	memset(parser, sizeof(parser), 0);
-	parser->buf = buf;
-	parser->len = len;
-	parser->state = PARSER_OK;
-	parser->on_capture = on_capture;
-	parser->on_mismatch = on_mismatch;
+	struct PEG *peg = xmalloc(sizeof(struct PEG));
+	peg->buf = buf;
+	peg->len = len;
+	peg->state = PARSER_OK;
+	peg->on_capture = on_capture;
+	peg->on_mismatch = on_mismatch;
+	return peg;
 }
 
 RULE(comment) {
-	CHAR('#', 1); THRU("\n");
+	if (CHAR('#'))
+	if (THRU("\n"))
+	return 1;
+	return 0;
+}
+
+RULE(digit) {
+	return CHARF(isdigit);
 }
 
 RULE(date) {
-	CHARF(isdigit, 4); CHAR('-', 1); CHARF(isdigit, 2); CHAR('-', 1); CHARF(isdigit, 2);
+	if (REPEAT(digit, 4))
+	if (CHAR('-'))
+	if (REPEAT(digit, 2))
+	if (CHAR('-'))
+	if (REPEAT(digit, 2))
+	return 1;
+	return 0;
 }
 
-
 RULE(entry) {
-	TO("|"); CHAR('|', 1); TO("|"); CHAR('|', 1); MATCH(date); CHAR('|', 1); TO("\n"); CHAR('\n', 1);
+	if (TO("|"))
+	if (CHAR('|'))
+	if (TO("|"))
+	if (CHAR('|'))
+	if (MATCH(date))
+	if (CHAR('|'))
+	if (TO("\n"))
+	if (CHAR('\n'))
+	return 1;
+	return 0;
 }
 
 RULE(entry_comment) { // (+ :comment :entry)
-	TRY_MATCH(comment);
-	TRY_MATCH(entry);
+	if (!MATCH(comment))
+	if (!MATCH(entry))
+	return 0;
+	return 1;
 }
 
 RULE(moved_grammar) {
-	ANY(entry_comment);
-	EOS();
+	if (ANY(entry_comment))
+	if (EOS())
+	return 1;
+	return 0;
 }
 
 void
-on_capture(struct Parser *parser, const char *rule, const char *buf, size_t len, void *userdata)
+on_capture(struct PEG *peg, const char *rule, const char *buf, size_t len, void *userdata)
 {
 	char *s = strndup(buf, len);
 	printf("captured in %s: %s\n", rule, s);
@@ -293,31 +297,35 @@ on_capture(struct Parser *parser, const char *rule, const char *buf, size_t len,
 }
 
 void
-on_mismatch(struct Parser *parser, const char *rule, void *userdata)
+on_mismatch(struct PEG *peg, const char *rule, void *userdata)
 {
 	printf("mismatch %s\n", rule);
 }
 
 #include "ipv4.c"
-
-RULE(foo) {
-	MATCH(byte);
-	printf("here\n");
-	EOS();
+RULE(foobar) {
+	if (MATCH(byte))
+	if (EOS())
+	return 1;
+	return 0;
 }
 
 int
 main(int argc, char *argv[])
 {
-	const char *buf = "#\naudio/polypaudio|audio/pulseaudio|2008-01-01|Project renamed\n# asdjflasdk\n"
-	"audio/akode-plugins-polypaudio||2008-01-01|Polypaudio is obsolete in favor of Pulseaudio\n"
-"audio/akode-plugins-polypaudio||2008-01-01|Polypaudio is obsolete in favor of Pulseaudio\n";
-	struct Parser *parser = parser_new(buf, strlen(buf), on_capture, on_mismatch);
-	//moved_grammar(parser);
-	printf("parser state: %d\n", parser->state);
+	const char *buf;
+	int state;
+	//= "#\naudio/polypaudio|audio/pulseaudio|2008-01-01|Project renamed\n# asdjflasdk\n"
+// 	"audio/akode-plugins-polypaudio||2008-01-01|Polypaudio is obsolete in favor of Pulseaudio\n"
+// "audio/akode-plugins-polypaudio||2008-01-01|Polypaudio is obsolete in favor of Pulseaudio\n";
+	buf = "# asfdsa\naudio/akode-plugins-polypaudio||2008-01-01|Polypaudio is obsolete in favor of Pulseaudio\n";
+	struct PEG *peg = peg_new(buf, strlen(buf), on_capture, on_mismatch);
+	state = moved_grammar(peg);
+	printf("peg state: %s\n", state ? "matched" : "mismatch");
+
 	buf = "256";
-	parser = parser_new(buf, strlen(buf), on_capture, on_mismatch);
-	foo(parser);
-	printf("parser state: %d\n", parser->state);
+	peg = peg_new(buf, strlen(buf), on_capture, on_mismatch);
+	state = foobar(peg);
+	printf("peg state: %s\n", state ? "matched" : "mismatch");
 	return 0;
 }
