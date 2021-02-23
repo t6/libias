7afcd33322e676d6ffa653e86d7b48fad3d5f064
Tobias Kortkamp <t@tobik.me>
2021-02-23 13:47:13 +0100

    lint: Do not lint files from submodules or untracked files

--- tests/lint.sh
+++ tests/lint.sh
@@ -1,27 +1,37 @@
 #!/bin/sh
 set -u
-ROOT="${PWD}"
 CPPCHECK="cppcheck"
 MANDOC="mandoc"
 
 lint_failed=0
 lint_run=0
 
-cd "${ROOT}"
-if type ${CPPCHECK} >/dev/null 2>&1; then
-lint_run=$((lint_run + 1))
-srcs="$(find . -name '*.c' -and -not -name 'tests.c')"
-if ! ${CPPCHECK} --error-exitcode=1 --library=posix --quiet --force --inconclusive ${srcs}; then
-	lint_failed=$((lint_failed + 1))
-fi
+echo -n "cppcheck: "
+srcs="$(git ls-files | grep '\.c$' | grep -v '^tests.c$')"
+if [ -z "${srcs}" ] || ! type ${CPPCHECK} >/dev/null 2>&1; then
+	echo "skipped"
+else
+	lint_run=$((lint_run + 1))
+	if ${CPPCHECK} --error-exitcode=1 --library=posix --quiet --force --inconclusive ${srcs}; then
+		echo "ok"
+	else
+		echo
+		lint_failed=$((lint_failed + 1))
+	fi
 fi
 
-cd "${ROOT}/man"
-if type ${MANDOC} >/dev/null 2>&1; then
-lint_run=$((lint_run + 1))
-if ! ${MANDOC} -Tlint -Wstyle ./*.1; then
-	lint_failed=$((lint_failed + 1))
-fi
+echo -n "mandoc: "
+mans="$(git ls-files | grep '\.1$')"
+if [ -z "${mans}" ] || ! type ${MANDOC} >/dev/null 2>&1; then
+	echo "skipped"
+else
+	lint_run=$((lint_run + 1))
+	if ${MANDOC} -Tlint -Wstyle ${mans}; then
+		echo "ok"
+	else
+		echo
+		lint_failed=$((lint_failed + 1))
+	fi
 fi
 
 printf "fail: %s ok: %s/%s\n" "${lint_failed}" "$((lint_run - lint_failed))" "${lint_run}"
